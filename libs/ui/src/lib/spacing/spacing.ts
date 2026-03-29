a3d8eef417749a38a2f6b368ac6030b3540f1d1e
YasmimVieira <vieiray937@gmail.com>
2026-03-29 12:01:54 -0300

    feat: add spacing

--- libs/ui/src/lib/spacing/spacing.ts
+++ libs/ui/src/lib/spacing/spacing.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+
+export type SpacingSize = '4xs' | '3xs' | '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
 
 @Component({
-  selector: 'lib-spacing',
-  imports: [],
+  selector: 'luster-spacing',
+  imports: [CommonModule],
   templateUrl: './spacing.html',
   styleUrl: './spacing.scss',
 })
-export class Spacing {}
+export class Spacing {
+  size = input<SpacingSize>('md');
+  direction = input<'horizontal' | 'vertical' | 'all'>('all');
+
+  classes = computed(() => {
+    const sizeClass = `luster-spacing--${this.size()}`;
+    const dirClass = `luster-spacing--${this.direction()}`;
+    return `${sizeClass} ${dirClass}`.trim();
+  });
+}
